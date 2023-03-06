f16a9ba81a505f10f9d676412d34536a151f7a7c
llghdud921 <llghdud921@naver.com>
2023-03-07 02:34:31 +0900

    [feat] DiffableDataSource를 이용하여 BookCollectionView 구현 (#9)
    - HomeBookSection 추가

--- OdockOdock/OdockOdock/Presentation/Feature/HomeList/HomeListViewController.swift
+++ OdockOdock/OdockOdock/Presentation/Feature/HomeList/HomeListViewController.swift
@@ -7,13 +7,70 @@
 
 import UIKit
 
-class HomeListViewController: UIViewController {
+final class HomeListViewController: BaseViewController {
 
+    let bookCollectionView = BookCollectionView()
+    
+    typealias BookDataSource = UICollectionViewDiffableDataSource<HomeBookSection, Book>
+    var bookDataSource: BookDataSource? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureCollectionViewDataSource()
     }
-
-
+    
+    override func setupView() {
+        super.setupView()
+        
+        view.addSubview(bookCollectionView)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+        
+        bookCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            bookCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            bookCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bookCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bookCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
 
+// MARK: - Book CollectionView
+
+private extension HomeListViewController {
+    
+    func configureCollectionViewDataSource() {
+        let BookCollectionViewRegistration = configureBookCollectionViewCellRegistration()
+        bookDataSource = UICollectionViewDiffableDataSource<HomeBookSection, Book>(collectionView: bookCollectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, item: Book) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: BookCollectionViewRegistration, for: indexPath, item: item)
+        }
+        
+        applySnapShotBookCollectionView()
+    }
+    
+    func configureBookCollectionViewCellRegistration() -> UICollectionView.CellRegistration<BookCollectionViewCell, Book> {
+        return UICollectionView.CellRegistration<BookCollectionViewCell, Book> { (cell, indexPath, item) in
+            cell.label.text = item.text
+            cell.bookImageView.image = item.image
+        }
+    }
+    
+    func applySnapShotBookCollectionView() {
+        var snapshot = NSDiffableDataSourceSnapshot<HomeBookSection, Book>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems([Book(text: "test", image: UIImage(systemName: "pencil") ?? UIImage()),
+                              Book(text: "test", image: UIImage(systemName: "pencil") ?? UIImage()),
+                              Book(text: "test", image: UIImage(systemName: "pencil") ?? UIImage()),
+                              Book(text: "test", image: UIImage(systemName: "pencil") ?? UIImage()),
+                              Book(text: "test", image: UIImage(systemName: "pencil") ?? UIImage()),
+                              Book(text: "test", image: UIImage(systemName: "pencil") ?? UIImage()),
+                              Book(text: "test", image: UIImage(systemName: "pencil") ?? UIImage())])
+        bookDataSource?.apply(snapshot, animatingDifferences: false)
+    }
+}
---
+++ OdockOdock/OdockOdock/Presentation/Feature/HomeList/SubViews/HomeBookSection.swift
@@ -0,0 +1,14 @@
+//
+//  HomeBookSection.swift
+//  OdockOdock
+//
+//  Created by 이호영 on 2023/03/07.
+//
+
+import Foundation
+
+/// 책 정보를 나타내는 CollectionView Section
+///
+enum HomeBookSection {
+    case main
+}
